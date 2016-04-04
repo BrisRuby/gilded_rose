6d8c52fc45d1b19a0bc636176ed2fbda9f9a8860
ridget <tom.ridge@tworedkites.com>
2016-04-04 14:22:03 +1000

    extract step 2

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -82,6 +82,28 @@ class PassEvaluator < ItemEvaluator
     end
   end
 
+  def step_2
+    self.sell_in -= 1
+  end
+
+  def step_3
+    if sell_in < 0
+      if name != 'Aged Brie'
+        if name != 'Backstage passes to a TAFKAL80ETC concert'
+          if quality > 0
+            self.quality -= 1
+          end
+        else
+          self.quality = quality - quality
+        end
+      else
+        if quality < 50
+          self.quality += 1
+        end
+      end
+    end
+  end
+
   def process
     step_1
     step_2
